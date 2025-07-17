eab5c39b805bcd4dd0cbd8588783005f1837db33
Clemens Helm <clemens.helm@gmail.com>
2025-07-17 15:12:07 +0200

    Use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_17_111042) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
